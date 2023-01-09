f9373fbccf3dcb30a04fa49b9267f9cd0cf24df2
Kevin <kevin@700brands.com>
2023-01-09 08:48:21 +0800

    create feed formik

--- src/components/CreateFeed.tsx
+++ src/components/CreateFeed.tsx
@@ -1,30 +1,43 @@
 import { faImage, faVideo } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Form, Formik } from "formik";
 import Button from "./Button";
+import TextAreaField from "./Form/TextAreaField";
 
 const CreateFeed = () => {
   return (
     <div className="flex flex-col rounded-md bg-dark w-full shadowm-sm overflow-hidden">
-      <textarea
-        className="w-full p-5 bg-dark text-light outline-none"
-        name="body"
-        placeholder="What's happening?"
-        rows={1}
-      />
-      <div className="p-5 flex flex-row items-center justify-between text-light">
-        <div>
-          <Button>Post</Button>
-        </div>
-        <div className="gap-5 flex">
-          <button>
-            <FontAwesomeIcon icon={faImage} />
-          </button>
-          <button>
-            <FontAwesomeIcon icon={faVideo} />
-          </button>
-          <span>Drag and drop</span>
-        </div>
-      </div>
+      <Formik
+        initialValues={{ body: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <TextAreaField
+              className="w-full p-5 bg-dark text-light outline-none"
+              name="body"
+              placeholder="What's happening?"
+              rows={1}
+            />
+            <div className="p-5 flex flex-row items-center justify-between text-light">
+              <div>
+                <Button isSubmitting={isSubmitting}>Post</Button>
+              </div>
+              <div className="gap-5 flex">
+                <button>
+                  <FontAwesomeIcon icon={faImage} />
+                </button>
+                <button>
+                  <FontAwesomeIcon icon={faVideo} />
+                </button>
+                <span>Drag and drop</span>
+              </div>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
--- src/components/Form/TextAreaField.tsx
+++ src/components/Form/TextAreaField.tsx
@@ -1,21 +1,21 @@
 import { faWarning } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ErrorMessage, useField } from "formik";
-import { InputHTMLAttributes } from "react";
+import { useField } from "formik";
+import { TextareaHTMLAttributes } from "react";
 
-type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
+type InputFieldProps = TextareaHTMLAttributes<HTMLTextAreaElement> & {
   label?: string;
   name: string;
   placeholder?: string;
 };
 
-const InputField = ({ label, placeholder, ...props }: InputFieldProps) => {
+const TextAreaField = ({ label, placeholder, ...props }: InputFieldProps) => {
   const [field, { error }] = useField(props);
 
   return (
     <div className="flex flex-col">
       {label && <label className="capitalize">{label}</label>}
-      <input
+      <textarea
         className="mt-2 block py-2 px-2 bg-light border border-red-500 placeholder:capitalize"
         style={{ border: !!error ? "1px solid rgb(239 68 68)" : "none" }}
         id={field.name}
@@ -33,4 +33,4 @@ const InputField = ({ label, placeholder, ...props }: InputFieldProps) => {
   );
 };
 
-export default InputField;
+export default TextAreaField;
