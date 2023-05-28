79b5737c8cc5a0e2900b237ed75d71374c0b5168
Kevin <kevin@700brands.com>
2023-05-28 11:53:03 +0800

    meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,10 +4,19 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Poster Asia</title>
+        <meta
+          name="description"
+          content="A lightweight social media app, welcome!"
+          key="desc"
+        />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
